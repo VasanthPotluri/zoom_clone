f41c0fb7f1a585d34124313d3447f4ed01c84ab9
VasanthPotluri <srivasanthpotluri@gmail.com>
2025-03-15 17:17:34 +0530

    Fixed Vercel deployment issues

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
@@ -10,7 +10,7 @@ import Loader from "@/components/Loader";
 
 const MeetingPage = () => {  
   const { id } = useParams();
-  const { user, isLoaded } = useUser();
+  const { isLoaded } = useUser(); // ✅ Removed 'user' since it's unused
   const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   const [call, isCallLoading] = useGetCallById(id ?? ""); // ✅ Now correctly receives an array
@@ -30,4 +30,5 @@ const MeetingPage = () => {
 };
 
 export default MeetingPage;
+
 
